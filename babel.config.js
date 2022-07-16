132f3cfdf9d977826bd93b2e85ddc86be66d6bf9
Joshua <12107070+Joshua-Bomba@users.noreply.github.com>
2022-07-16 15:30:00 -0400

    plugin updates

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,43 @@
+const lazyImports = require('metro-react-native-babel-preset/src/configs/lazy-imports');
+
 module.exports = {
 	ignore: ['node_modules/detox'],
 	plugins: [
+		'@babel/plugin-transform-flow-strip-types',
+		'@babel/plugin-proposal-optional-catch-binding',
+		[
+			'@babel/plugin-proposal-class-properties',
+			{
+				loose: true
+			}
+		],
+		'@babel/plugin-syntax-dynamic-import',
+		'@babel/plugin-syntax-export-default-from',
+		'@babel/plugin-transform-react-jsx',
+		'@babel/plugin-transform-sticky-regex',
+		'@babel/plugin-transform-unicode-regex',
+		'@babel/plugin-proposal-export-default-from',
+		[
+			'@babel/plugin-transform-modules-commonjs',
+			{
+				allowTopLevelThis: true, // dont rewrite global `this` -> `undefined`
+				lazy: importSpecifier => lazyImports.has(importSpecifier),
+				strict: false,
+				strictMode: false // prevent "use strict" injections
+			}
+		],
+		'@babel/plugin-proposal-object-rest-spread',
+		'metro-react-native-babel-preset/src/transforms/transform-symbol-member',
+		'@babel/plugin-transform-react-display-name',
+		'@babel/plugin-proposal-optional-chaining',
+		'@babel/plugin-proposal-nullish-coalescing-operator',
+		[
+			'@babel/plugin-transform-runtime',
+			{
+				helpers: true,
+				regenerator: true
+			}
+		],
 		[
 			'rewrite-require',
 			{
@@ -36,5 +73,5 @@ module.exports = {
 		],
 		['./scripts/rewrite-node-global']
 	],
-	presets: ['module:metro-react-native-babel-preset']
+	presets: []
 };
